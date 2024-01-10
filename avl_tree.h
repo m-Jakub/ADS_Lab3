20f5797d31d62edc1e6b3b031773b9752c54a81f
m-Jakub <xkubaxmx@gmail.com>
2024-01-10 13:31:51 +0100

    assignment and copy

--- avl_tree.h
+++ avl_tree.h
@@ -12,21 +12,42 @@ private:
         Info info;
         Node *left;
         Node *right;
+        int height;
 
         Node() : left(nullptr), right(nullptr) {}
         Node(const Key &key, const Info &info) : key(key), info(info), left(nullptr), right(nullptr) {}
     };
 
     Node *root;
-    
+
+    void search(const DataType &item, bool &found, BinNodePointer &locptr, BinNodePointer &parent);
     void inorder(ostream &out, Node *subtree) const;
     void graph(ostream &out, Node *subtree, int indent) const;
+    void left_rotation(Node *&subtree);
+    void right_rotation(Node *&subtree);
+    Node *copy(Node *subtree)
+    {
+        if (subtree == nullptr)
+            return nullptr;
+        Node *newNode = new Node(subtree->key, subtree->info);
+        newNode->left = copy(subtree->left);
+        newNode->right = copy(subtree->right);
+        newNode->height = subtree->height;
+    }
 
 public:
-    avl_tree();
-    avl_tree(const avl_tree &src);
-    ~avl_tree();
-    avl_tree &operator=(const avl_tree &src);
+    avl_tree() : root(nullptr) {}
+    avl_tree(const avl_tree &src) { root = copy(src.root); }
+    ~avl_tree() { clear(); }
+    avl_tree &operator=(const avl_tree &src)
+    {
+        if (this != &src)
+        {
+            clear();
+            root = copy(src.root);
+        }
+        return *this;
+    }
     bool insert(const Key &key, const Info &info);
     bool remove(const Key &key);
     bool search(const Key &key) const;
